d9a6f7aa0658c1150f2042b2fa63352bc1d20568
Catalina Musteata <musteatacatalina6@gmail.com>
2026-03-05 13:30:05 +0200

    .

--- test_custom_math.py
+++ test_custom_math.py
@@ -2,6 +2,20 @@ import unittest
 import custom_math
 
 class TestCustomMath(unittest.TestCase):
+
+    # Sarcina 1
+    def test_sum(self):
+        self.assertEqual(custom_math.sum(1, 3), 4)
+
+    # Sarcina 2
+    def test_division_valid(self):
+        self.assertEqual(custom_math.division(10, 2), 5)
+
+    def test_division_by_zero(self):
+        with self.assertRaises(ValueError):
+            custom_math.division(5, 0)
+
+    # Sarcina 3
     def test_sum_multiple(self):
         test_values = [
             (1, 1, 2),
@@ -12,12 +26,24 @@ class TestCustomMath(unittest.TestCase):
         for x, y, expected in test_values:
             self.assertEqual(custom_math.sum(x, y), expected)
 
-    def test_division(self):
-        self.assertEqual(custom_math.division(10, 2), 5)
+    def test_division_multiple(self):
+        test_values = [
+            (10, 2, 5),
+            (20, 4, 5),
+            (9, 3, 3)
+        ]
 
-    def test_division_by_zero(self):
-        with self.assertRaises(ValueError):
-            custom_math.division(5, 0)
+        for x, y, expected in test_values:
+            self.assertEqual(custom_math.division(x, y), expected)
+
+    # Sarcina 4
+    def test_positive_result(self):
+        result = custom_math.sum(5, 5)
+        self.assertTrue(result > 0)
+
+    def test_negative_result(self):
+        result = custom_math.sum(-5, -5)
+        self.assertFalse(result > 0)
 
 if __name__ == '__main__':
     unittest.main()
